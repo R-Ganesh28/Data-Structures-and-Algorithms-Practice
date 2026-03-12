1a419b8cd46032060eecea595d5c26c26d0af520
Ganesh. R <130210233+R-Ganesh28@users.noreply.github.com>
2026-03-12 23:59:15 +0530

    Hash Table + Math + String

---
+++ Lesson 01/Roman_To_Integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInteger(self, s:str) -> int:
+        map = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        ans = 0
+        for i in range(len(s) - 1):
+            if map[s[i]] < map[s[i + 1]]:
+                ans -= map[s[i]]
+            else:
+                ans += map[s[i]]
+        return ans + map[s[-1]]
+s = "LVIII"
+S1 = Solution()
+print(S1.romanToInteger(s))
