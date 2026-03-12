700945441161a974f6613a22c444144d252b64f4
Ganesh. R <130210233+R-Ganesh28@users.noreply.github.com>
2026-03-12 23:48:54 +0530

    if else

---
+++ Lesson 01/Fizz_Buzz.py
@@ -0,0 +1,16 @@
+class Solution:
+    def fizzBuzz(self, n:int) -> list[str]:
+        ans = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(i)
+        return ans
+nums = 15
+S1 = Solution()
+print(S1.fizzBuzz(nums))
